2a5edfaedb2324ef38f85e4ff16faa5fb8119028
vincentzhang15 <vincent.zhang0315@gmail.com>
2021-06-29 20:58:13 -0400

    Update MathTrainer.java
    Add more documentation and clean up code by removing unnecessary lines. Added quit program features.

--- ConsolePrograms/2020-04-20_MathTrainer/MathTrainer.java
+++ ConsolePrograms/2020-04-20_MathTrainer/MathTrainer.java
@@ -1,6 +1,15 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.List;
+import java.util.ArrayList;
 
+/**
+ * Class to store the values to be summed.
+ * 
+ * @author Vincent Zhang
+ * @since 20 April 2020
+ */
 class Store
 {
 	int a;
@@ -20,30 +29,24 @@ class Store
  */
 public class MathTrainer
 {
-	static void test(ArrayList<Store> data) throws IOException
+	/**
+	 * Run the program.
+	 *
+	 * @param args Command-line arguments
+	 * @return void
+	 */
+	public static void main (String [] args)
 	{
+		System.out.println("Enter \"QUIT\" to quit the program.");
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		for(int i = 0; i < data.size(); i++)
-		{
-			int random = (int)(Math.random() * data.size());
-			int a = data.get(random).a;
-			int b = data.get(random).b;
-			System.out.print(a + " + " + b + " = ");
-			
-			int n = Integer.parseInt(br.readLine());
-			if(n == (a+b))
-				System.out.print("    Correct");
-			else
-				System.out.print("    " + (a+b));
-			System.out.println();
-			data.remove(random);
-		}
-	}
-	public static void main (String [] args) throws IOException
-	{
-		ArrayList<Store> addition = new ArrayList<Store>();
+		List<Store> addition = new ArrayList<Store>();
+
+		exit: // Break nested loop.
 		for(;;)
 		{
+			// Ensures equal coverage of all single-digit additions by storing the combinations as a set of questions.
+			// Only until after the entire set of questions is completed, does the set refresh.
 			for(int a = 2; a <= 9; a++)
 			{
 				for(int b = a; b <= 9; b++)
@@ -52,7 +55,38 @@ public class MathTrainer
 					addition.add(s);
 				}
 			}
-			test(addition);
+
+			for(int i = 0; i < addition.size(); i++)
+			{
+				int random = (int)(Math.random() * addition.size());
+				int a = addition.get(random).a;
+				int b = addition.get(random).b;
+				System.out.print(a + " + " + b + " = ");
+				
+				int n = -1;
+				try
+				{
+					n = Integer.parseInt(br.readLine());
+				}
+				catch(NumberFormatException e)
+				{
+					// User enters anything that is not numbers.
+					System.out.println("Thank you for using the program.");
+					break exit;
+				}
+				catch(IOException e)
+				{
+					e.printStackTrace();
+				}
+
+				if(n == (a+b))
+					System.out.print("    Correct");
+				else
+					System.out.print("    " + (a+b));
+				System.out.println();
+				addition.remove(random);
+			}
+
 		}
 	}
 }
