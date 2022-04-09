746b1cbd85ce0cadde1c2e3b0a2ba4774fe579b9
vincentzhang15 <vincent.zhang0315@gmail.com>
2022-04-09 15:47:05 -0400

    Update CalculateGrade.py
    Add current average calculation.

--- ConsolePrograms/2021-11-30_CalculateGrade/CalculateGrade.py
+++ ConsolePrograms/2021-11-30_CalculateGrade/CalculateGrade.py
@@ -5,6 +5,7 @@ Section 1: Obtained %
 Section 2: Lost %
 Section 3: Undetermined %
 Section 4: Max Final Mark
+Section 5: Current Average
 
 @author Vincent Zhang
 @since 30 November 2021
@@ -37,11 +38,14 @@ subtotal = 0
 for d in data:
     obtained += float(d[0]) * float(d[1]) / 100
     subtotal += float(d[1]) # sum of weights
+if subtotal > 100: print(f"WARNING: total marks entered is: {subtotal}")
 lost = subtotal - obtained
 undetermined = 100 - subtotal
 max_final = 100 - lost
+average = obtained / (obtained + lost) * 100
 
 print(f"Obtained: \t{obtained:.3f}%")
 print(f"Lost: \t\t{lost:.3f}%")
 print(f"Undetermined: \t{undetermined:.3f}%")
 print(f"Max Final: \t{max_final:.3f}%")
+print(f"Average: \t{average:.3f}%")
